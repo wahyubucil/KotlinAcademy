98bc191aa01c1e4b1ae4a507facf7f40466dce08
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-25 01:39:34 +0800

    Create the layout

--- app/src/main/java/com/dicoding/kotlinacademy/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/MainActivity.kt
@@ -1,13 +1,56 @@
 package com.dicoding.kotlinacademy
 
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import com.dicoding.kotlinacademy.R.color.colorAccent
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinner = spinner()
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(colorAccent,
+                        android.R.color.holo_green_light,
+                        android.R.color.holo_orange_light,
+                        android.R.color.holo_red_light)
+
+                relativeLayout {
+                    lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar {
+                    }.lparams {
+                        centerHorizontally()
+                    }
+                }
+            }
+        }
     }
 
 }
