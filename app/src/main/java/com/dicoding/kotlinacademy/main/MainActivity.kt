103ec7d7a31f0c9647b0dfce9772b51801cfc703
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-25 02:57:21 +0800

    Completed football club app

--- app/src/main/java/com/dicoding/kotlinacademy/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/main/MainActivity.kt
@@ -5,21 +5,30 @@ import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import com.dicoding.kotlinacademy.R
 import com.dicoding.kotlinacademy.R.color.colorAccent
+import com.dicoding.kotlinacademy.api.ApiRepository
+import com.dicoding.kotlinacademy.model.Team
+import com.dicoding.kotlinacademy.util.invisible
+import com.dicoding.kotlinacademy.util.visible
+import com.google.gson.Gson
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
 
+    private var teams: MutableList<Team> = mutableListOf()
+    private lateinit var presenter: MainPresenter
+    private lateinit var adapter: MainAdapter
     private lateinit var listTeam: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var spinner: Spinner
-
+    private lateinit var leagueName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         linearLayout {
@@ -51,6 +60,47 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        val spinnerItems = resources.getStringArray(R.array.league)
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item,
+                spinnerItems)
+        spinner.adapter = spinnerAdapter
+
+        adapter = MainAdapter(teams)
+        listTeam.adapter = adapter
+
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = MainPresenter(this, request, gson)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                leagueName = spinner.selectedItem.toString()
+                presenter.getTeamList(leagueName)
+            }
+
+        }
+
+        swipeRefresh.onRefresh {
+            presenter.getTeamList(leagueName)
+        }
+    }
+
+    override fun showLoading() {
+        progressBar.visible()
+    }
+
+    override fun hideLoading() {
+        progressBar.invisible()
+    }
+
+    override fun showTeamList(data: List<Team>) {
+        swipeRefresh.isRefreshing = false
+        teams.clear()
+        teams.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 
 }
