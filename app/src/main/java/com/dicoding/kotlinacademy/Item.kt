563a77e0138e881f666b535a3d11a95a9ed50f68
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-24 13:57:41 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/kotlinacademy/Item.kt
@@ -0,0 +1,3 @@
+package com.dicoding.kotlinacademy
+
+data class Item (val name: String?, val image: Int?)
---
+++ app/src/main/java/com/dicoding/kotlinacademy/RecyclerViewAdapter.kt
@@ -0,0 +1,38 @@
+package com.dicoding.kotlinacademy
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+class RecyclerViewAdapter(private val context: Context, private val items: List<Item>,
+                          private val listener: (Item) -> Unit)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position], listener)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val name = view.findViewById<TextView>(R.id.name)
+        private val image = view.findViewById<ImageView>(R.id.image)
+
+        fun bindItem(item: Item, listener: (Item) -> Unit) {
+            name.text = item.name
+            Glide.with(itemView.context).load(item.image).into(image)
+            itemView.setOnClickListener {
+                listener(item)
+            }
+        }
+    }
+}
