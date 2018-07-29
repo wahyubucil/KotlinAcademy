be917cbd019161ebbcde61aa2f13c4cd59c03b9c
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-29 16:57:01 +0800

    Create the home layout

---
+++ app/src/main/java/com/dicoding/kotlinacademy/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlinacademy.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dicoding.kotlinacademy.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
