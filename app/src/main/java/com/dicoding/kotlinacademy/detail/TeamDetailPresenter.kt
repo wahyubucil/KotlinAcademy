68d21bc21837fe3f6fedb3e62708f0b38705c993
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-30 13:32:40 +0800

    Create getTeamDetail on presenter and api

---
+++ app/src/main/java/com/dicoding/kotlinacademy/detail/TeamDetailPresenter.kt
@@ -0,0 +1,28 @@
+package com.dicoding.kotlinacademy.detail
+
+import com.dicoding.kotlinacademy.api.ApiRepository
+import com.dicoding.kotlinacademy.api.TheSportDBApi
+import com.dicoding.kotlinacademy.model.TeamResponse
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class TeamDetailPresenter(private val view: TeamDetailView,
+                          private val apiRepository: ApiRepository,
+                          private val gson: Gson) {
+
+    fun getTeamDetail(teamId: String) {
+        view.showLoading()
+        doAsync {
+            val data = gson.fromJson(apiRepository
+                    .doRequest(TheSportDBApi.getTeamDetail(teamId)),
+                    TeamResponse::class.java
+            )
+
+            uiThread {
+                view.hideLoading()
+                view.showTeamDetail(data.teams)
+            }
+        }
+    }
+}
