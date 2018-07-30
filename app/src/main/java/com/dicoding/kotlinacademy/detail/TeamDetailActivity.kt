0c69068142aeb90471713bc7445fb7037264d23b
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-30 18:03:57 +0800

    Implement remove favorite teams

--- app/src/main/java/com/dicoding/kotlinacademy/detail/TeamDetailActivity.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/detail/TeamDetailActivity.kt
@@ -25,7 +25,10 @@ import com.dicoding.kotlinacademy.util.visible
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
@@ -110,12 +113,23 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
             }
         }
 
+        favoriteState()
         val request = ApiRepository()
         val gson = Gson()
         presenter = TeamDetailPresenter(this, request, gson)
         presenter.getTeamDetail(id)
         swipeRefresh.onRefresh {
             presenter.getTeamDetail(id)
+        }
+    }
+
+    private fun favoriteState() {
+        database.use {
+            val result = select(Favorite.TABLE_FAVORITE)
+                    .whereArgs("(TEAM_ID = {id})",
+                            "id" to id)
+            val favorite = result.parseList(classParser<Favorite>())
+            if (!favorite.isEmpty()) isFavorite = true
         }
     }
 
@@ -142,6 +156,7 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail_menu, menu)
         menuItem = menu
+        setFavorite()
         return true
     }
 
@@ -152,7 +167,11 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
                 true
             }
             R.id.add_to_favorite -> {
-                addToFavorite()
+                if (isFavorite) removeFromFavorite() else addToFavorite()
+
+                isFavorite = !isFavorite
+                setFavorite()
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -170,6 +189,26 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
             snackbar(swipeRefresh, "Added to favorite").show()
         } catch (e: SQLiteConstraintException) {
             snackbar(swipeRefresh, e.localizedMessage).show()
+        }
+    }
+
+    private fun removeFromFavorite() {
+        try {
+            database.use {
+                delete(Favorite.TABLE_FAVORITE, "(TEAM_ID = {id})",
+                        "id" to id)
+            }
+            snackbar(swipeRefresh, "Removed from favorite").show()
+        } catch (e: SQLiteConstraintException) {
+            snackbar(swipeRefresh, e.localizedMessage).show()
+        }
+    }
+
+    private fun setFavorite() {
+        if (isFavorite) {
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_added_to_favorites)
+        } else {
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_add_to_favorites)
         }
     }
 
